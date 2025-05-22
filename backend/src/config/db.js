2813366ccddd495c35f49e7fa08a23b797a29a4e
tuankhanh19405 <khanhptph49306@gmail.com>
2025-05-22 11:10:49 +0700

    cart,favorite,reviews

---
+++ backend/src/config/db.js
@@ -0,0 +1,22 @@
+// db.js
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const mongoURI = process.env.MONGO_URI;
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(mongoURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("✅ Đã kết nối MongoDB thành công");
+  } catch (err) {
+    console.error("❌ Lỗi khi kết nối MongoDB:", err.message);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
