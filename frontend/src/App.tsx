5ea0161777e3d5dfb8bafcd0c091d8dbbd652550
kiennt <kienntph48528@gmail.com>
2025-05-29 15:21:52 +0700

    chore: setup TailwindCSS and fix routing issue with BrowserRouter

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,51 +1,16 @@
-// import { Route, Routes } from "react-router";
-
-// import { AuthProvider } from './context/AuthContext';
-// import RegisterPage from "./pages/RegisterPage";
-// import LayoutAdmin from "./layouts/LayoutAdmin";
-// import Dashboard from "./pages/Dashboard";
-
-
-
-// function App() {
-//   return (
-//       <AuthProvider>
-//      <>
-//       <Routes>
-//         <Route path="/" element={<LayoutAdmin />}>
-//         <Route path="/register" element={<RegisterPage/>} />
-//           <Route index element={<Dashboard />} />
-//         </Route>
-//       </Routes>
-//     </> 
-//     </AuthProvider>
-//   );
-// }
-
-// export default App;
-
-
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import { ConfigProvider } from 'antd';
 
-// Contexts
 import { CartProvider } from './context/CartContext';
 import { AuthProvider } from './context/AuthContext';
 
-
-// Layout
 import Layout from './components/layout/Layout';
-
-// Pages
-
 import RegisterPage from './pages/RegisterPage';
 import LayoutAdmin from './layouts/LayoutAdmin';
 import Dashboard from './pages/Dashboard';
 import LoginPage from './pages/LoginPage';
 
-
-// Ant Design theme customization
 const theme = {
   token: {
     colorPrimary: '#B45309',
@@ -59,19 +24,14 @@ function App() {
     <ConfigProvider theme={theme}>
       <AuthProvider>
         <CartProvider>
-          
-            <Router>
-              <Layout>
-                <Routes>
-                  <Route path="/admin" element={<LayoutAdmin />}></Route>
-                   <Route index element={<Dashboard />} />
-                  <Route path="/register" element={<RegisterPage />} />
-                    <Route path="/login" element={<LoginPage />} />
-            
-                </Routes>
-              </Layout>
-            </Router>
-          
+          <Layout>
+            <Routes>
+              <Route path="/admin" element={<LayoutAdmin />} />
+              <Route index element={<Dashboard />} />
+              <Route path="/register" element={<RegisterPage />} />
+              <Route path="/login" element={<LoginPage />} />
+            </Routes>
+          </Layout>
         </CartProvider>
       </AuthProvider>
     </ConfigProvider>
