1a0e89fe85a288a232e796f2efcbfcc3a6009463
phuongdmph <phuongdmph51181@gmail.com>
2025-07-05 09:51:04 +0700

    Fix HomePage

--- backend/src/routers/index.js
+++ backend/src/routers/index.js
@@ -9,6 +9,17 @@ import bannersRouter from "./banners.router.js";
 import couponsRouter from "./coupons.router.js";
 import cartRouter from "./cart.router.js";
 import cartItemRouter from "./cartItem.router.js";
+import careInstructionsRouter from "./careInstructions.router.js";
+import assembliesRouter from "./assemblies.router.js";
+import orderItemRouter from "./orderItem.router.js";
+
+
+import productVariantRouter from "./productVariant.router.js";
+import materialsRouter from "./materials.router.js";
+
+
+import productVariantRouter from "./productVariant.router.js";
+import materialsRouter from "./materials.router.js";
 
 const router = Router();
 
@@ -20,7 +31,16 @@ router.use("/reviews", reviewRouter);
 router.use("/api/favorites", favoriteRouter); 
 router.use("/api/banners", bannersRouter); 
 router.use("/api/coupons", couponsRouter);
-router.use("/cart", cartRouter);
-router.use("/cartItem", cartItemRouter);
+router.use("/api/cart", cartRouter);
+router.use("/api/cartItem", cartItemRouter);
+router.use("api/careInstructions", careInstructionsRouter);
+router.use("/api/assemblies", assembliesRouter);
+router.use("/api/oderItems", orderItemRouter);
+
+router.use("/api/product-variants", productVariantRouter);
+router.use("/api/materials", materialsRouter);
+
+router.use("/api/product-variants", productVariantRouter);
+router.use("/api/materials", materialsRouter);
 
 export default router;
