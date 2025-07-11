aad8d262c279e37a9fdda7b9b2b48b27bcf72edf
nguyentaituananh <anhnttph48578@gmail.com>
2025-07-11 23:03:04 +0700

    update layout và App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,7 @@ import { StrictMode } from "react";
 
 // Layouts
 import LayoutAdmin from "./layouts/admin/LayoutAdmin";
-import Layout from "./components/layout/layout";
+import Layout from "./components/layout/Layout";
 
 // Context Providers
 import { CartProvider } from "./context/CartContext";
@@ -57,11 +57,14 @@ function App() {
                   <ReviewProvider>
                     <Routes>
                       {/* ADMIN ROUTES */}
-                      <Route path="/admin" element={
-                        <ProtectedRouteAdmin>
-                          <LayoutAdmin />
-                        </ProtectedRouteAdmin>
-                      }>
+                      <Route
+                        path="/admin"
+                        element={
+                          <ProtectedRouteAdmin>
+                            <LayoutAdmin />
+                          </ProtectedRouteAdmin>
+                        }
+                      >
                         <Route index element={<DashboardPage />} />
                         <Route path="dashboard" element={<DashboardPage />} />
                         <Route path="product" element={<ProductList />} />
@@ -84,8 +87,14 @@ function App() {
                       <Route path="/" element={<Layout />}>
                         <Route index element={<HomePage />} />
                         <Route path="products" element={<ProductsPage />} />
-                        <Route path="products/:category" element={<ProductsPage />} />
-                        <Route path="product/:id" element={<ProductDetailPage />} />
+                        <Route
+                          path="products/:category"
+                          element={<ProductsPage />}
+                        />
+                        <Route
+                          path="product/:id"
+                          element={<ProductDetailPage />}
+                        />
                         <Route path="cart" element={<CartPage />} />
                         <Route path="checkout" element={<CheckoutPage />} />
                         <Route path="about" element={<AboutPage />} />
