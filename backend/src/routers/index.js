3e12fdbbf3bb27a84eae3370da234527980e01ec
tuankhanh19405 <khanhptph49306@gmail.com>
2025-07-20 23:04:01 +0700

    upadte db

--- backend/src/routers/index.js
+++ backend/src/routers/index.js
@@ -4,6 +4,14 @@ import authRouter from './auth/index.js'
 import userRouter from './user/index.js'
 import uploadRouter from './upload/index.js'
 import categoryRouter from './category/index.js'
+import addressesRouter from './addresses/index.js'
+import messageRouter from './message/index.js'
+// import paymentRouter from './payment/index.js'
+import notificationRouter from './notification/index.js'
+import instructionRouter from './instruction/index.js'
+import delivery_optionRouter from './delivery_option/index.js'
+import product_variantsRouter from './product_variants/index.js'
+// Khởi tạo router chính
 
 const router = Router()
 
@@ -14,5 +22,13 @@ router.use('/api/auth', authRouter)
 router.use('/api/users', userRouter)
 router.use('/api/upload', uploadRouter)
 router.use('/api/categories', categoryRouter)
+router.use('/api/addresses', addressesRouter)
+router.use('/api/message', messageRouter)
+// router.use('/api/payment', paymentRouter)
+router.use('/api/notification', notificationRouter)
+router.use('/api/instructions', instructionRouter)
+router.use('/api/delivery-options', delivery_optionRouter)
+router.use('/api/product-variants', product_variantsRouter)
+
 
 export default router
