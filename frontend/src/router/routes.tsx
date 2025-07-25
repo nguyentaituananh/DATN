2cb66006eb01742d0c7c253c5d02655f6c1c1ab8
nguyentaituananh <anhnttph48578@gmail.com>
2025-07-25 13:49:25 +0700

    ...

--- frontend/src/router/routes.tsx
+++ frontend/src/router/routes.tsx
@@ -1,37 +1,43 @@
-import { routes } from '@/constants'
-import { LayoutType } from '@/constants/enum'
-import { CategoryManagerPage, DashboardPage, ProductManagerPage, HomePage, LoginPage } from '@/features'
-import type { CustomRouteConfig } from '@/types'
+import { routes } from "@/constants";
+import { LayoutType } from "@/constants/enum";
+import {
+  CategoryManagerPage,
+  DashboardPage,
+  ProductManagerPage,
+  HomePage,
+  LoginPage,
+} from "@/features";
+import type { CustomRouteConfig } from "@/types";
 
 export const routeConfigs: CustomRouteConfig[] = [
-	{
-		path: routes.home,
-		element: <HomePage />,
-		layout: LayoutType.DEFAULT,
-		protected: true
-	},
-	{
-		path: routes.login,
-		element: <LoginPage />,
-		layout: LayoutType.DEFAULT,
-		protected: false
-	},
-	{
-		path: routes.dashboard,
-		element: <DashboardPage />,
-		layout: LayoutType.DASHBOARD,
-		protected: true
-	},
-	{
-		path: routes.categoryManagement,
-		element: <CategoryManagerPage />,
-		layout: LayoutType.DASHBOARD,
-		protected: true
-	},
-	{
-		path: routes.productManagement,
-		element: <ProductManagerPage />,
-		layout: LayoutType.DASHBOARD,
-		protected: true
-	}
-]
+  {
+    path: routes.home,
+    element: <HomePage />,
+    layout: LayoutType.DEFAULT,
+    protected: true,
+  },
+  {
+    path: routes.login,
+    element: <LoginPage />,
+    layout: LayoutType.DEFAULT,
+    protected: false,
+  },
+  {
+    path: routes.dashboard,
+    element: <DashboardPage />,
+    layout: LayoutType.DASHBOARD,
+    protected: true,
+  },
+  {
+    path: routes.categoryManagement,
+    element: <CategoryManagerPage />,
+    layout: LayoutType.DASHBOARD,
+    protected: true,
+  },
+  {
+    path: routes.productManagement,
+    element: <ProductManagerPage />,
+    layout: LayoutType.DASHBOARD,
+    protected: true,
+  },
+];
